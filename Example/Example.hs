4702b549a69134c77acbeaba610c0ab362f8a438
Neil Mitchell <ndmitchell@gmail.com>
2007-01-27 21:29:30 +0000

    Add the MSNBC example

--- Example/Example.hs
+++ Example/Example.hs
@@ -25,4 +25,29 @@ haskellHitCount = do
                 num = ss !! (i - 1)
                 Just i = findIndex (== "times.") ss
                 ss = words s
-                TagText s = sections (isOpenTagName "p") x !! 1 !! 1
+                TagText s = sections (isTagOpenName "p") x !! 1 !! 1
+
+
+
+msnbcTechNews :: IO ()
+msnbcTechNews = do
+        src <- openURL "http://www.msnbc.msn.com/"
+        let tags = parseTags src
+        let headlines = concatMap f $ sections (~== TagOpen "table" [("width","420")]) tags
+        mapM_ putStrLn $ zipWith (\i s -> show i ++ ") " ++ [' '|i<=9] ++ s) [1..] headlines
+    where
+        f :: [Tag] -> [String]
+        f tags = case head (sections (isTagOpenName "b") tags) of
+                    xs@(_:TagText s:_)
+                        | any ("TECH AND SCIENCE" `isPrefixOf`) (tails s)
+                        -> g $ takeWhile (~/= TagOpen "table" [("width","420")]) xs
+                    _ -> []
+
+        g :: [Tag] -> [String]
+        g xs = map h $ sections (~== TagOpen "td" [("valign","top")]) xs
+        
+        h :: [Tag] -> String
+        h x = fromTagText $ head $ dropWhile (not . isTagText) $ dropWhile (not . isTagOpenName "a") x
+
+
+
