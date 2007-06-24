b397c44dcb7d0f85c03ae1ead8279261d6d775d8
Neil Mitchell <ndmitchell@gmail.com>
2007-06-24 11:57:48 +0000

    Move the parser out into a separate module

--- Text/HTML/TagSoup/Parser.hs
+++ Text/HTML/TagSoup/Parser.hs
@@ -1,99 +1,298 @@
-module Text.HTML.TagSoup.Parser (
-   Parser, Status(Status),
-   char, dropSpaces, eof, force, getPos,
-   many, many1, many1Satisfy, manySatisfy, readUntil,
-   satisfy, source, string,
-   emit,
-   eval, write, gets, mfix)
-  where
 
+module Text.HTML.TagSoup.Parser(
+    -- * Data structures and parsing
+    PosTag, CharType, Parser,
+    parseTags, parsePosTags, parseFilePosTags,
+    parseTag, parseInnerOfTag
+    ) where
 
-import qualified Text.HTML.TagSoup.Position as Position
+import Text.HTML.TagSoup.Parser.All
+   (char, dropSpaces, force, getPos,
+    many, many1, many1Satisfy, readUntil,
+    satisfy, string,
+    emit, mfix)
+
+import qualified Text.HTML.TagSoup.Parser.All as Parser
+import qualified Text.HTML.TagSoup.Entity as HTMLEntity
+
 import Text.HTML.TagSoup.Position (Position)
+import Text.HTML.TagSoup.Type
 
-import Text.HTML.TagSoup.Parser.Custom
--- import Text.HTML.TagSoup.Parser.MTL
+import Control.Monad (mplus, msum, when, liftM)
 
-import Control.Monad (mplus, liftM, liftM2)
-import Control.Monad.Fix (mfix)
-
-import Data.Char (isSpace)
-
-
-write :: FilePath -> Parser w () -> String -> Maybe [w]
-write fileName p =
-   fmap (\ ~(_,_,ws) -> ws) .
-   run p .
-   Status (Position.initialize fileName)
-
-eval :: FilePath -> Parser w a -> String -> Maybe a
-eval fileName p =
-   fmap (\ ~(x,_,_) -> x) .
-   run p .
-   Status (Position.initialize fileName)
+import Data.Char (isAlphaNum, isDigit, toLower, toUpper, chr)
+import Data.List (tails, groupBy)
+import Data.Maybe (fromMaybe, mapMaybe)
 
 
+type PosTag char = (Position, Tag char)
 
-eof :: Parser w Bool
-eof = gets (null . source)
-
-getPos :: Parser w Position
-getPos = gets sourcePos
-
-satisfy :: (Char -> Bool) -> Parser w Char
-satisfy p =
-   do c <- nextChar
-      if p c
-        then return c
-        else fail "character not matched"
-
--- | does never fail
-many :: Parser w a -> Parser w [a]
-many x =
-   {- It is better to have 'force' at the place it is,
-      instead of writing it to the recursive call,
-      because 'x' can cause an infinite loop. -}
-   force $ mplus (many1 x) (return [])
-
-many1 :: Parser w a -> Parser w [a]
-many1 x = liftM2 (:) x (many x)
-
-manySatisfy :: (Char -> Bool) -> Parser w String
-manySatisfy = ignoreEmit . many . satisfy
-
-many1Satisfy :: (Char -> Bool) -> Parser w String
-many1Satisfy = ignoreEmit . many1 . satisfy
-
-dropSpaces :: Parser w ()
-dropSpaces =
-   ignoreEmit $ liftM (const ()) $ manySatisfy isSpace
+type Parser char a = Parser.Parser (PosTag char) a
 
 
-char :: Char -> Parser w Char
-char c = satisfy (c==)
-
-string :: String -> Parser w String
-string = ignoreEmit . mapM char
-
-
-readUntil :: String -> Parser w (Bool,String)
-readUntil pattern =
-   let recurse =
-          force $
-          liftM (const (True,[])) (string pattern)
-          `mplus`
-          do isEOF <- eof
-             if isEOF
-               then return (False,[])
-               else liftM2
-                       (\c ~(found,str) -> (found,c:str))
-                       nextChar recurse
-   in  ignoreEmit recurse
-{-
-runStateT (readUntil "-->") (Position.initialize "input", "<!-- comment --> other stuff")
+{- |
+Parse a single tag, throws an error if there is a syntax error.
+This is useful for parsing a match pattern.
 -}
+parseTag :: (CharType char, Show char) =>
+   String -> Tag char
+parseTag str =
+   let tags =
+          fromMaybe (error "tagEqualElement: parse should never fail") $
+          Parser.write "string" parsePosTag str
+       throwError = error $
+          "parseTag: parsing results in\n" ++
+          unlines (map show tags)
+   in  case tags of
+          [(_,tag)] ->
+              if isTagWarning tag
+                then throwError
+                else tag
+          _ -> throwError
+
+{- |
+Parse the inner of a single tag.
+That is, @parseTag \"\<bla\>\"@ is the same as @parseInnerOfTag \"\<bla\>\"@.
+-}
+parseInnerOfTag :: (CharType char, Show char) =>
+   String -> Tag char
+parseInnerOfTag str = parseTag $ "<"++str++">"
 
 
 
-emit :: w -> Parser w ()
-emit w = tell [w]
+parseFilePosTags :: CharType char => FilePath -> String -> [PosTag char]
+parseFilePosTags fileName =
+   fromMaybe (error "parseFilePosTag can never fail.") .
+   Parser.write fileName (many parsePosTag >> return ())
+
+
+-- | Parse an HTML document to a list of 'Tag'.
+-- Automatically expands out escape characters.
+parsePosTags :: CharType char => String -> [PosTag char]
+parsePosTags =
+   fromMaybe (error "parsePosTag can never fail.") .
+   Parser.write "input" (many parsePosTag >> return ())
+
+-- | Like 'parsePosTags' but hides source file positions.
+parseTags :: CharType char => String -> [Tag char]
+parseTags = map snd . parsePosTags
+
+
+parsePosTag :: CharType char => Parser char ()
+parsePosTag = do
+   pos <- getPos
+   msum $
+    (do char '<'
+        msum $
+         parseSpecialTag pos :
+         parseCloseTag pos :
+         parseOpenTag pos :
+         (do emitTag pos (TagText [fromChar '<'])
+             emitWarning pos "A '<', that is not part of a tag. Encode it as &lt; please."
+         ) :
+         []
+    ) :
+    parseText pos :
+    []
+
+
+parseOpenTag :: CharType char => Position -> Parser char ()
+parseOpenTag pos =
+   do name <- parseName
+      dropSpaces
+      mfix
+         (\attrs ->
+            emit (pos, TagOpen name attrs) >>
+            many parseAttribute)
+      force $ msum $
+        (do closePos <- getPos
+            string "/>"
+            emitTag closePos (TagClose name)) :
+        (do junkPos <- getPos
+            readUntilTerm
+               (\ junk ->
+                  emitWarningWhen
+                     (not $ null junk)
+                     junkPos ("Junk in opening tag: \"" ++ junk ++ "\""))
+               ("Unterminated open tag \"" ++ name ++ "\"") ">") :
+        []
+
+parseCloseTag :: Position -> Parser char ()
+parseCloseTag pos =
+   do char '/'
+      name <- parseName
+      emitTag pos (TagClose name)
+      dropSpaces
+      junkPos <- getPos
+      readUntilTerm
+         (\ junk ->
+            emitWarningWhen
+               (not $ null junk)
+               junkPos ("Junk in closing tag: \"" ++ junk ++"\""))
+         ("Unterminated closing tag \"" ++ name ++"\"") ">"
+
+parseSpecialTag :: Position -> Parser char ()
+parseSpecialTag pos =
+   do char '!'
+      msum $
+       (do string "--"
+           readUntilTerm
+              (\ cmt -> emitTag pos (TagComment cmt))
+              "Unterminated comment" "-->") :
+       (do name <- many1Satisfy isAlphaNum
+           dropSpaces
+           readUntilTerm
+              (\ info -> emitTag pos (TagSpecial name info))
+              ("Unterminated special tag \"" ++ name ++ "\"") ">") :
+       []
+
+parseText :: CharType char => Position -> Parser char ()
+parseText pos =
+   mfix
+     (\ text ->
+        emitTag pos (TagText text) >>
+        parseString1 ('<'/=))
+     >> return ()
+
+
+parseAttribute :: CharType char => Parser char (Attribute char)
+parseAttribute =
+   do name <- parseName
+      dropSpaces
+      value <-
+         force $
+         mplus
+            (string "=" >> dropSpaces >> parseValue)
+            (return [])
+      dropSpaces
+      return (name, value)
+
+parseName :: Parser char String
+parseName =
+   many1Satisfy (\c -> isAlphaNum c || c `elem` "_-:")
+
+parseValue :: CharType char => Parser char [char]
+parseValue =
+   force $ msum $
+      parseQuoted "Unterminated doubly quoted value string" '"' :
+      parseQuoted "Unterminated singly quoted value string" '\'' :
+      parseUnquotedValue :
+      []
+
+parseUnquotedValueChar :: Parser Char String
+parseUnquotedValueChar =
+   let parseValueChar =
+          do pos <- getPos
+             str <- parseChar (not . flip elem " >\"\'")
+             let wrong = filter (not . isValidValueChar) str
+             emitWarningWhen
+                (not (null wrong))
+                pos $ "Illegal characters in unquoted value: " ++ wrong
+             return str
+   in  liftM concat $ many parseValueChar
+
+parseUnquotedValueHTMLChar :: Parser HTMLChar [HTMLChar]
+parseUnquotedValueHTMLChar =
+   let parseValueChar =
+          do pos <- getPos
+             hc <- parseHTMLChar (not . flip elem " >\"\'")
+             let wrong =
+                    case hc of
+                       Char c -> not (isValidValueChar c)
+                       _ -> False
+             emitWarningWhen wrong pos $
+                "Illegal characters in unquoted value: '" ++ show wrong ++ "'"
+             return hc
+   in  many parseValueChar
+
+isValidValueChar :: Char -> Bool
+isValidValueChar c  =  isAlphaNum c || c `elem` "_-:."
+
+parseQuoted :: CharType char => String -> Char -> Parser char [char]
+parseQuoted termMsg quote =
+   do char quote
+      str <- parseString (quote/=)
+      force $ mplus
+         (char quote >> return ())
+         (do termPos <- getPos
+             emitWarning termPos termMsg)
+      return str
+
+{-
+Instead of using 'generateTag' we could also wrap the call to 'readUntilTerm'
+in 'mfix' in order to emit a tag, where some information is read later.
+-}
+readUntilTerm ::
+   (String -> Parser char ()) -> String -> String -> Parser char ()
+readUntilTerm generateTag termWarning termPat =
+   do ~(termFound,str) <- readUntil termPat
+      generateTag str
+      termPos <- getPos
+      emitWarningWhen (not termFound) termPos termWarning
+
+
+class CharType char where
+   fromChar :: Char -> char
+   parseString  :: (Char -> Bool) -> Parser char [char]
+   parseString1 :: (Char -> Bool) -> Parser char [char]
+   parseUnquotedValue :: Parser char [char]
+
+instance CharType Char where
+   fromChar = id
+   parseString  p = liftM concat $ many  (parseChar p)
+   parseString1 p = liftM concat $ many1 (parseChar p)
+   parseUnquotedValue = parseUnquotedValueChar
+
+instance CharType HTMLChar where
+   fromChar = Char
+   parseString  p = many  (parseHTMLChar p)
+   parseString1 p = many1 (parseHTMLChar p)
+   parseUnquotedValue = parseUnquotedValueHTMLChar
+
+
+parseChar :: (Char -> Bool) -> Parser char String
+parseChar p =
+   do pos <- getPos
+      x <- parseHTMLChar p
+      let returnChar c = return $ c:[]
+      case x of
+         Char c -> returnChar c
+         NumericRef num -> returnChar (chr num)
+         NamedRef name ->
+            maybe
+               (let refName = '&':name++";"
+                in  emitWarning pos ("Unknown HTML entity " ++ refName) >>
+                    return refName)
+               (returnChar . chr)
+               (lookup name HTMLEntity.table)
+
+
+parseHTMLChar :: (Char -> Bool) -> Parser char HTMLChar
+parseHTMLChar p =
+   do pos <- getPos
+      c <- satisfy p
+      if c=='&'
+        then
+          force $
+          mplus
+            (do ent <-
+                   mplus
+                      (char '#' >>
+                       liftM (NumericRef . read) (many1Satisfy isDigit))
+                      (liftM NamedRef $ many1Satisfy isAlphaNum)
+                char ';'
+                return ent)
+            (emitWarning pos "Ill formed entity" >>
+             return (Char '&'))
+        else return (Char c)
+
+
+emitWarningWhen :: Bool -> Position -> String -> Parser char ()
+emitWarningWhen cond pos msg =
+   force $ when cond $ emitWarning pos msg
+
+emitWarning :: Position -> String -> Parser char ()
+emitWarning pos msg = emitTag pos (TagWarning msg)
+
+emitTag :: Position -> Tag char -> Parser char ()
+emitTag = curry emit
