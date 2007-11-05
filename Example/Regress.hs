5c87d0f9fa3accf9036e429146d15ddcb15d80ea
Neil Mitchell <ndmitchell@gmail.com>
2007-11-05 16:19:37 +0000

    Remove the lazyWarnings tests (only one worked, and that was a bit dubious anyway) - replace it with proper functional tests

--- Example/Regress.hs
+++ Example/Regress.hs
@@ -15,14 +15,14 @@ instance Show (Test a) where
     show x = x `seq` "All tests passed"
 pass :: Test ()
 pass = Pass
-
+a === b = if a == b then pass else fail $ "Does not equal: " ++ show a ++ " =/= " ++ show b
 
 -- * The Main section
 
 regress :: IO ()
 regress = print $ do
+    parseTests
     lazyTags == lazyTags `seq` pass
-    lazyWarnings == lazyWarnings `seq` pass
     matchCombinators
 
 
@@ -56,25 +56,13 @@ lazyTags =
       (cycle "1<2 ") :
       
       -- need further analysis
-      --("<html name="++cycle "val&ue") :
-      --("<html name="++cycle "va&l!ue") :
-      --("&" ++ cycle "t") :
+      ("<html name="++cycle "val&ue") :
+      ("<html name="++cycle "va&l!ue") :
+      ("&" ++ cycle "t") :
 
       -- i don't see how this can work unless the junk gets into the AST?
       --("</html "++cycle "junk") :
 
-      []
-
-lazyWarnings :: [Char]
-lazyWarnings =
-   map ((!!1000) . show . tail . parseTags) $
-      (repeat '&') :
-      --("<html "++cycle "na!me=value ") :
-      --("<html name="++cycle "val!ue") :
-      --("<html name="++cycle "val&ue") :
-      --("<html name="++cycle "va&l!ue") :
-      --("</html "++cycle "junk") :
-      --(cycle "1<2 ") :
       []
 
 
@@ -96,3 +84,17 @@ matchCombinators = assert (and tests) pass
                   (TagOpen "table" [("id", "other name")])) :
             []
 
+
+parseTests :: Test ()
+parseTests = do
+    parseTags "<!DOCTYPE TEST>" === [TagOpen "!DOCTYPE" [("TEST","")]]
+    parseTags "<test \"foo bar\">" === [TagOpen "test" [("","foo bar")]]
+    parseTags "<test \'foo bar\'>" === [TagOpen "test" [("","foo bar")]]
+    parseTags "hello &amp; world" === [TagText "hello & world"]
+    parseTags "hello &#64; world" === [TagText "hello @ world"]
+    parseTags "hello &haskell; world" === [TagText "hello &haskell; world"]
+
+    parseTags "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">" ===
+        [TagOpen "!DOCTYPE" [("HTML",""),("PUBLIC",""),("","-//W3C//DTD HTML 4.01//EN"),("","http://www.w3.org/TR/html4/strict.dtd")]]
+    parseTags "<script src=\"http://edge.jobthread.com/feeds/jobroll/?s_user_id=100540&subtype=slashdot\">" ===
+        [TagOpen "script" [("src","http://edge.jobthread.com/feeds/jobroll/?s_user_id=100540&subtype=slashdot")]]
