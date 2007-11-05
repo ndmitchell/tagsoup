53b3ea606e32bead8875ffe8b97d2ed264fe66ad
Neil Mitchell <ndmitchell@gmail.com>
2007-11-05 15:40:14 +0000

    Change the validate program to still work

--- Example/Validate.hs
+++ Example/Validate.hs
@@ -5,14 +5,20 @@ Detects basic syntax errors like isolated ampersands and unquoted attribute valu
 module Main where
 
 import Text.HTML.TagSoup
-import Text.HTML.TagSoup.Position
 import System.Environment
 
 
 validate :: FilePath -> String -> String
-validate file input = unlines
-    [toReportText (setFileName file pos) ++ " " ++ msg
-    | TagPos pos (TagWarning msg) <- parseTagsGeneric input :: [TagPos Char]]
+validate file = unlines . f . parseTagsOptions opts
+    where
+        opts = options{optTagPosition=True, optTagWarning=True}
+
+        f (TagPosition row col:TagWarning warn:rest) =
+            ("Warning (" ++ show row ++ "," ++ show col ++ "): " ++ warn) : f rest
+        f (TagWarning warn:rest) =
+            ("Warning (?,?): " ++ warn) : f rest
+        f (x:xs) = f xs
+        f [] = []
 
 validateIO :: FilePath -> IO ()
 validateIO file =
