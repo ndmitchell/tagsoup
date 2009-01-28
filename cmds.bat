56521a3095aeee118d5dcc836ffef08f0df47a58
Neil Mitchell <ndmitchell@gmail.com>
2009-01-28 16:36:58 +0000

    Add the dump command to cmds

--- cmds.bat
+++ cmds.bat
@@ -12,6 +12,7 @@ goto end
 
 :profile
 ghc --make Main -odir obj\p -hidir obj\p -O -o -prof -auto-all tagsoup_p.exe
+del tagsoup_p_*.prof
 tagsoup_p.exe time +RTS -p
 ren tagsoup_p.prof tagsoup_p_time.prof
 tagsoup_p.exe timefile ketil.xml +RTS -p
@@ -20,7 +21,18 @@ goto end
 
 
 :dump
-echo dump here
+ghc --make Main -odir obj\o -hidir obj\o -O -o tagsoup_o.exe
+call %0 core Text\StringLike.hs stringlike
+call %0 core Text\HTML\TagSoup\Parser2.hs parser
+call %0 core Text\ParserCombinators\LazyParse.hs lazyparse
+goto end
+
+:core
+shift
+copy %1 obj\o\%1
+pushd obj\o
+ghc %1 -ddump-simpl -c > ..\..\%2.core
+popd
 goto end
 
 
