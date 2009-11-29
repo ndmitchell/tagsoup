ef92a76e0cf363c3368e000bc13a6ee1bbf0291f
Neil Mitchell <ndmitchell@gmail.com>
2009-11-29 18:28:48 +0000

    Desugar - flatten all matches

--- TagSoup/Generate/Desugar.hs
+++ TagSoup/Generate/Desugar.hs
@@ -33,10 +33,21 @@ untyped = map (descendBi untyped) . filter (not . isTypeSig)
 
 
 core :: [Decl] -> [Decl]
-core = transformBi removeWhere
+core = transformBi flatMatches . transformBi removeWhere
     where
+        -- afterwards, no where
         removeWhere (Match a b c d bod (BDecls whr)) | whr /= [] = Match a b c d (f bod) (BDecls [])
             where f (UnGuardedRhs x) = UnGuardedRhs $ Let (BDecls whr) x
                   f (GuardedRhss xs) = GuardedRhss [GuardedRhs a b $ Let (BDecls whr) c | GuardedRhs a b c <- xs]
         removeWhere x = x
 
+        -- afterwards, no multiple equations for one function
+        flatMatches (FunBind xs@(Match a b c d e f:_)) | length xs > 1 =
+                FunBind [Match a b (map PVar ps) Nothing (UnGuardedRhs bod) (BDecls [])]
+            where
+                ps = map (Ident . (++) "p_" . show) [1..length c]
+                bod = Case (Tuple $ map (Var . UnQual) ps) [Alt sl (PTuple p) (f b) d | Match _ _ p _ b d <- xs]
+                f (UnGuardedRhs x) = UnGuardedAlt x
+                f (GuardedRhss xs) = GuardedAlts $ map g xs
+                g (GuardedRhs x y z) = GuardedAlt x y z
+        flatMatches x = x
