995fb9e667110752cafff750a3226afd2bd35ba8
Neil Mitchell <ndmitchell@gmail.com>
2010-05-05 07:22:30 +0000

    Add fix for numeric references in attributes, before it could drop the remainder

--- TagSoup/Test.hs
+++ TagSoup/Test.hs
@@ -117,6 +117,8 @@ parseTests = do
     parseTags "hello &haskell; world" === [TagText "hello &haskell; world"]
     parseTags "hello \n\t world" === [TagText "hello \n\t world"]
     parseTags "<a href=http://www.google.com>" === [TagOpen "a" [("href","http://www.google.com")]]
+    parseTags "<foo bar=\"bar&#54;baz\">" === [TagOpen "foo" [("bar","bar6baz")]]
+    parseTags "<foo bar=\"bar&amp;baz\">" === [TagOpen "foo" [("bar","bar&baz")]]
 
     -- real cases reported by users
     parseTags "<a href=\"series.php?view=single&ID=72710\">" === [TagOpen "a" [("href","series.php?view=single&ID=72710")]]
