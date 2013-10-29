cb2a3ecebf0a1cb29cd13257caad0086e793178b
Neil Mitchell <ndmitchell@gmail.com>
2013-10-29 12:57:53 +0000

    Update the SPJ papers example

--- TagSoup/Sample.hs
+++ TagSoup/Sample.hs
@@ -88,8 +88,8 @@ spjPapers :: IO ()
 spjPapers = do
         tags <- fmap parseTags $ openItem "http://research.microsoft.com/en-us/people/simonpj/"
         let links = map f $ sections (~== "<A>") $
-                    takeWhile (~/= "<A name=haskell>") $
-                    drop 5 $ dropWhile (~/= "<A name=current>") tags
+                    takeWhile (~/= "<a name=haskell>") $
+                    drop 5 $ dropWhile (~/= "<a name=current>") tags
         putStr $ unlines links
     where
         f :: [Tag String] -> String
