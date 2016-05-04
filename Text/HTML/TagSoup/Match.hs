757d0bb75a04bb9c4bb21c37105f1ccf4f0951ec
Maximilian Tagher <feedback.tagher@gmail.com>
2016-05-04 16:18:07 -0700

    Add Haddock examples for matching tags

--- Text/HTML/TagSoup/Match.hs
+++ Text/HTML/TagSoup/Match.hs
@@ -1,42 +1,113 @@
 -- | Combinators to match tags. Some people prefer to use @(~==)@ from
 --   "Text.HTML.TagSoup", others prefer these more structured combinators.
 --   Which you use is personal preference.
-
+--
+-- The functions below offer maximum flexibility for matching tags.
+-- Using 'tagOpen', for example, you can match all links or buttons that have the "btn" class.
+--
+-- For simple uses cases—like matching all comment tags, or matching opening @\<a>@ tags,
+-- use the tag identification functions in "Text.HTML.TagSoup#tag-identification".
 module Text.HTML.TagSoup.Match where
 
 import Text.HTML.TagSoup.Type (Tag(..), Attribute)
 import Data.List (tails)
 
+-- * Matching Tags
 
--- | match an opening tag
+-- | Match an opening tag
+--
+-- ==== __Examples__
+--
+-- /Matching an opening @\<a>@ tag with a @"btn"@ class:/
+--
+-- >>> let tag = TagOpen "a" [("class", "btn")]
+-- >>> tagOpen (== "a") (\attrs -> any (== ("class", "btn")) attrs) tag
+-- True
 tagOpen :: (str -> Bool) -> ([Attribute str] -> Bool) -> Tag str -> Bool
 tagOpen pName pAttrs (TagOpen name attrs) =
    pName name && pAttrs attrs
 tagOpen _ _ _ = False
 
--- | match an closing tag
+-- | Match a closing tag
+--
+-- ==== __Examples__
+--
+-- /Matching a closing @\<\/a>@ tag:/
+--
+-- >>> tagClose (== "a") (TagClose "a")
+-- True
+--
+-- >>> tagClose (== "a") (TagOpen "a" [])
+-- False
 tagClose :: (str -> Bool) -> Tag str -> Bool
 tagClose pName (TagClose name) = pName name
 tagClose _ _ = False
 
--- | match a text
+-- | Match text tags
+--
+-- ==== __Examples__
+--
+-- /Match all text tags:/
+--
+-- >>> let tags = parseTags "<p>This is a paragraph</p>"
+-- [TagOpen "p" [],TagText "This is a paragraph",TagClose "p"]
+-- >>> filter (tagText (const True)) tags
+-- [TagText "This is a paragraph"]
 tagText :: (str -> Bool) -> Tag str -> Bool
 tagText p (TagText text) = p text
 tagText _ _ = False
 
+-- | Match comment tags
+--
+-- ==== __Examples__
+--
+-- /Matching comment tags that include an exclamation mark:/
+--
+-- >>> let tags = parseTags "<!--This is a comment-->"
+-- [TagComment "This is a comment!"]
+-- >>> all (tagComment (\s -> '!' `elem` s)) tags
+-- True
 tagComment :: (str -> Bool) -> Tag str -> Bool
 tagComment p (TagComment text) = p text
 tagComment _ _ = False
 
 
--- | match a opening tag's name literally
+-- | Match an opening tag's name literally
+--
+-- ==== __Examples__
+--
+-- /Matching @\<a>@ tags with the @id@ "foo":/
+--
+-- >>> let tag = TagOpen "a" [("id", "foo")]
+-- TagOpen "a" [("id","foo")]
+-- >>> tagOpenLit "a" (\attrs -> any (== ("id", "foo")) attrs) tag
+-- True
+--
 tagOpenLit :: Eq str => str -> ([Attribute str] -> Bool) -> Tag str -> Bool
 tagOpenLit name = tagOpen (name==)
 
--- | match a closing tag's name literally
+-- | Match a closing tag's name literally
+--
+-- ==== __Examples__
+--
+-- /Match a closing @\<a>@ tag:/
+--
+-- >>> tagCloseLit "a" (TagClose "a")
+-- True
+--
+-- >>> tagCloseLit "a" (TagClose "em")
+-- False
 tagCloseLit :: Eq str => str -> Tag str -> Bool
 tagCloseLit name = tagClose (name==)
 
+-- | Match an opening tag's name literally, and at least one of its attributes
+--
+-- ==== __Examples__
+--
+-- /Matching a @\<div>@ tag with the @id@ "foo":/
+--
+-- >>> tagOpenAttrLit "div" ("id", "foo") (TagOpen "div" [("id", "foo")])
+-- True
 tagOpenAttrLit :: Eq str => str -> Attribute str -> Tag str -> Bool
 tagOpenAttrLit name attr =
    tagOpenLit name (anyAttrLit attr)
@@ -46,6 +117,14 @@ Match a tag with given name, that contains an attribute
 with given name, that satisfies a predicate.
 If an attribute occurs multiple times,
 all occurrences are checked.
+
+==== __Examples__
+
+/Matching an @\<a>@ tag with an ID that starts with "comment-":/
+
+>>> let commentTag = TagOpen "a" [("id", "comment-45678")]
+>>> tagOpenAttrNameLit "a" "id" (\idValue -> "comment-" `Data.List.isPrefixOf` idValue) commentTag
+True
 -}
 tagOpenAttrNameLit :: Eq str => str -> str -> (str -> Bool) -> Tag str -> Bool
 tagOpenAttrNameLit tagName attrName pAttrValue =
@@ -54,15 +133,35 @@ tagOpenAttrNameLit tagName attrName pAttrValue =
 
 
 -- | Check if the 'Tag str' is 'TagOpen' and matches the given name
+--
+-- ==== __Examples__
+--
+-- /Matching an @\<a>@ tag:/
+--
+-- >>> tagOpenNameLit "a" (TagOpen "a" [])
+-- True
+--
+-- >>> tagOpenNameLit "a" (TagOpen "div" [])
+-- False
 tagOpenNameLit :: Eq str => str -> Tag str -> Bool
 tagOpenNameLit name = tagOpenLit name (const True)
 
 -- | Check if the 'Tag str' is 'TagClose' and matches the given name
+--
+-- ==== __Examples__
+--
+-- /Matching a closing @\<\/a>@ tag:/
+--
+-- >>> tagCloseNameLit "a" (TagClose "a")
+-- True
+--
+-- >>> tagCloseNameLit "a" (TagClose "div")
+-- False
 tagCloseNameLit :: Eq str => str -> Tag str -> Bool
 tagCloseNameLit name = tagCloseLit name
 
 
-
+-- * Matching attributes
 
 anyAttr :: ((str,str) -> Bool) -> [Attribute str] -> Bool
 anyAttr = any
